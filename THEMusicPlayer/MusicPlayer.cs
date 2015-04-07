b67ea85c3028c74626dc860ea361613a135bae5b
Sandilya Jandhyala <jysandilya@gmail.com>
2015-04-07 22:34:52 +0530

    Untabified

--- THEMusicPlayer/MusicPlayer.cs
+++ THEMusicPlayer/MusicPlayer.cs
@@ -12,145 +12,145 @@ using Windows.Media;
 
 namespace THEMusicPlayer
 {
-	public class MusicPlayer
-	{
-		private MediaElement mediaElement_;
-		private SystemMediaTransportControls mediaControls_;
-		private List<string> nowPlayingList = new List<string>();
-		private int currentTrackIndex_;
-		private CoreDispatcher dispatcher_;
+    public class MusicPlayer
+    {
+        private MediaElement mediaElement_;
+        private SystemMediaTransportControls mediaControls_;
+        private List<string> nowPlayingList = new List<string>();
+        private int currentTrackIndex_;
+        private CoreDispatcher dispatcher_;
 
-		public MusicPlayer(MediaElement medElement, CoreDispatcher disp)
-		{
-			this.dispatcher_ = disp;
-			this.mediaElement_ = medElement;
-			//This should be set in XAML, but just to be sure...
-			mediaElement_.AudioCategory = AudioCategory.BackgroundCapableMedia;
+        public MusicPlayer(MediaElement medElement, CoreDispatcher disp)
+        {
+            this.dispatcher_ = disp;
+            this.mediaElement_ = medElement;
+            //This should be set in XAML, but just to be sure...
+            mediaElement_.AudioCategory = AudioCategory.BackgroundCapableMedia;
 
-			mediaElement_.CurrentStateChanged += mediaElement__CurrentStateChanged;
-			mediaElement_.MediaEnded += mediaElement__MediaEnded;
+            mediaElement_.CurrentStateChanged += mediaElement__CurrentStateChanged;
+            mediaElement_.MediaEnded += mediaElement__MediaEnded;
 
-			InitializeTransportControls();
-		}
+            InitializeTransportControls();
+        }
 
-		private void InitializeTransportControls()
-		{
-			mediaControls_ = SystemMediaTransportControls.GetForCurrentView();
-			mediaControls_.ButtonPressed += mediaControls__ButtonPressed;
+        private void InitializeTransportControls()
+        {
+            mediaControls_ = SystemMediaTransportControls.GetForCurrentView();
+            mediaControls_.ButtonPressed += mediaControls__ButtonPressed;
 
-			mediaControls_.IsPlayEnabled = true;
-			mediaControls_.IsPauseEnabled = true;
-		}
+            mediaControls_.IsPlayEnabled = true;
+            mediaControls_.IsPauseEnabled = true;
+        }
 
-		private void mediaElement__CurrentStateChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-		{
-			switch (mediaElement_.CurrentState)
-			{
+        private void mediaElement__CurrentStateChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            switch (mediaElement_.CurrentState)
+            {
 
-				case MediaElementState.Playing:
-					mediaControls_.PlaybackStatus = MediaPlaybackStatus.Playing;
-					break;
-				case MediaElementState.Paused:
-					mediaControls_.PlaybackStatus = MediaPlaybackStatus.Paused;
-					break;
-				case MediaElementState.Stopped:
-					mediaControls_.PlaybackStatus = MediaPlaybackStatus.Stopped;
-					break;
-				case MediaElementState.Closed:
-					mediaControls_.PlaybackStatus = MediaPlaybackStatus.Closed;
-					break;
-				default:
-					break;
-			}
-		}
+                case MediaElementState.Playing:
+                    mediaControls_.PlaybackStatus = MediaPlaybackStatus.Playing;
+                    break;
+                case MediaElementState.Paused:
+                    mediaControls_.PlaybackStatus = MediaPlaybackStatus.Paused;
+                    break;
+                case MediaElementState.Stopped:
+                    mediaControls_.PlaybackStatus = MediaPlaybackStatus.Stopped;
+                    break;
+                case MediaElementState.Closed:
+                    mediaControls_.PlaybackStatus = MediaPlaybackStatus.Closed;
+                    break;
+                default:
+                    break;
+            }
+        }
 
-		private void mediaControls__ButtonPressed(SystemMediaTransportControls sender, SystemMediaTransportControlsButtonPressedEventArgs args)
-		{
-			switch (args.Button)
-			{
-				case SystemMediaTransportControlsButton.Play:
-					PlayMedia();
-					break;
-				case SystemMediaTransportControlsButton.Pause:
-					PauseMedia();
-					break;
-				default:
-					break;
-			}
-		}			
+        private void mediaControls__ButtonPressed(SystemMediaTransportControls sender, SystemMediaTransportControlsButtonPressedEventArgs args)
+        {
+            switch (args.Button)
+            {
+                case SystemMediaTransportControlsButton.Play:
+                    PlayMedia();
+                    break;
+                case SystemMediaTransportControlsButton.Pause:
+                    PauseMedia();
+                    break;
+                default:
+                    break;
+            }
+        }			
 
-		private void mediaElement__MediaEnded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-		{
-			currentTrackIndex_++;
-			if (currentTrackIndex_ < nowPlayingList.Count)
-			{
-				UpdateMediaSource();
-				PlayMedia();
-			}
-		}
+        private void mediaElement__MediaEnded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            currentTrackIndex_++;
+            if (currentTrackIndex_ < nowPlayingList.Count)
+            {
+                UpdateMediaSource();
+                PlayMedia();
+            }
+        }
 
-		private async void UpdateMediaSource()
-		{
-			var file = await Windows.Storage.StorageFile.GetFileFromPathAsync(nowPlayingList[currentTrackIndex_]);
-			var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
-			mediaElement_.SetSource(stream, "audio/x-mpeg-3");
-		}
+        private async void UpdateMediaSource()
+        {
+            var file = await Windows.Storage.StorageFile.GetFileFromPathAsync(nowPlayingList[currentTrackIndex_]);
+            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            mediaElement_.SetSource(stream, "audio/x-mpeg-3");
+        }
 
-		public async void PlayMedia()
-		{
-			await dispatcher_.RunAsync(CoreDispatcherPriority.Normal, () =>
-				{
-					mediaElement_.Play();
-				});
-		}
+        public async void PlayMedia()
+        {
+            await dispatcher_.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    mediaElement_.Play();
+                });
+        }
 
-		public async void PauseMedia()
-		{
-			await dispatcher_.RunAsync(CoreDispatcherPriority.Normal, () =>
-				{
-					mediaElement_.Pause();
-				});
-		}
+        public async void PauseMedia()
+        {
+            await dispatcher_.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    mediaElement_.Pause();
+                });
+        }
 
-		public async void StopMedia()
-		{
-			await dispatcher_.RunAsync(CoreDispatcherPriority.Normal, () =>
-				{
-					mediaElement_.Stop();
-				});
-		}
+        public async void StopMedia()
+        {
+            await dispatcher_.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    mediaElement_.Stop();
+                });
+        }
 
-		public void EnqueueTrack(string filePath)
-		{
-			nowPlayingList.Add(filePath);
-			if (nowPlayingList.Count == 1)
-			{
-				currentTrackIndex_ = 0;
-				UpdateMediaSource();
-			}
-			else
-			{
-				currentTrackIndex_++;
-			}
-		}
+        public void EnqueueTrack(string filePath)
+        {
+            nowPlayingList.Add(filePath);
+            if (nowPlayingList.Count == 1)
+            {
+                currentTrackIndex_ = 0;
+                UpdateMediaSource();
+            }
+            else
+            {
+                currentTrackIndex_++;
+            }
+        }
 
-		public void RemoveTrackAtIndex(int index)
-		{
-			if (index == currentTrackIndex_)
-			{
-				StopMedia();
-				nowPlayingList.RemoveAt(index);
-				UpdateMediaSource();
-			}
-			else if (index < currentTrackIndex_)
-			{
-				nowPlayingList.RemoveAt(index);
-				currentTrackIndex_--;
-			}
-			else if (index > currentTrackIndex_)
-			{
-				nowPlayingList.RemoveAt(index);
-			}
-		}
-	}
+        public void RemoveTrackAtIndex(int index)
+        {
+            if (index == currentTrackIndex_)
+            {
+                StopMedia();
+                nowPlayingList.RemoveAt(index);
+                UpdateMediaSource();
+            }
+            else if (index < currentTrackIndex_)
+            {
+                nowPlayingList.RemoveAt(index);
+                currentTrackIndex_--;
+            }
+            else if (index > currentTrackIndex_)
+            {
+                nowPlayingList.RemoveAt(index);
+            }
+        }
+    }
 }
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -13,29 +13,29 @@ namespace UnitTests
     [TestClass]
     public class UnitTest1
     {
-		/// <summary>
-		/// This tests whether the Music Player works or not. It is limited to
-		/// just testing whether the methods work/don't throw exceptions unexpectedly,
-		/// since audio needs to be manually tested.
-		/// </summary>
-		[UITestMethod]
-		public void PlaybackTest()
-		{
-			const string testfile = @"C:\Users\jysan_000\Music\[2014] The Black Market\04 - The Black Market.mp3";
-			var mediaElement = new Windows.UI.Xaml.Controls.MediaElement();
-			var dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
-			var musicPlayer = new THEMusicPlayer.MusicPlayer(mediaElement, dispatcher);
-			musicPlayer.EnqueueTrack(testfile);
-			musicPlayer.PlayMedia();
-			Task.Delay(TimeSpan.FromSeconds(5)).Wait();
-			musicPlayer.PauseMedia();
-			Task.Delay(TimeSpan.FromSeconds(2)).Wait();
-			musicPlayer.PlayMedia();
-			Task.Delay(TimeSpan.FromSeconds(3)).Wait();
-			musicPlayer.StopMedia();
-			musicPlayer.PlayMedia();
-			Task.Delay(TimeSpan.FromSeconds(2)).Wait();
-			musicPlayer.StopMedia();
-		}
+        /// <summary>
+        /// This tests whether the Music Player works or not. It is limited to
+        /// just testing whether the methods work/don't throw exceptions unexpectedly,
+        /// since audio needs to be manually tested.
+        /// </summary>
+        [UITestMethod]
+        public void PlaybackTest()
+        {
+            const string testfile = @"C:\Users\jysan_000\Music\[2014] The Black Market\04 - The Black Market.mp3";
+            var mediaElement = new Windows.UI.Xaml.Controls.MediaElement();
+            var dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+            var musicPlayer = new THEMusicPlayer.MusicPlayer(mediaElement, dispatcher);
+            musicPlayer.EnqueueTrack(testfile);
+            musicPlayer.PlayMedia();
+            Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+            musicPlayer.PauseMedia();
+            Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+            musicPlayer.PlayMedia();
+            Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+            musicPlayer.StopMedia();
+            musicPlayer.PlayMedia();
+            Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+            musicPlayer.StopMedia();
+        }
     }
 }
