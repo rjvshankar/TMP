5189df2edde1e77f4bf9c7e372b243edca54a5c9
rjvshankar <rjvshankar@github.com>
2015-04-17 12:30:37 +0530

    Added Collection and SongData class

---
+++ THEMusicPlayer/Collection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace THEMusicPlayer
+{
+    public class Collection
+    {
+        private List<SongData> songList_ = new List<SongData>();
+
+        public static void GetSongList()
+        {
+            
+        }
+
+        public static void Save()
+        {
+
+        }
+
+        public static void Refresh()
+        {
+
+        }
+
+    }
+}
---
+++ THEMusicPlayer/SongData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THEMusicPlayer
+{
+    public class SongData
+    {
+        public String path;
+        public String title;
+        public String album;
+        public String artist;
+
+        public SongData()
+        {
+            path = null;
+            title = null;
+            album = null;
+            artist = null;
+        }
+
+        public SongData(String a, String b, String c, String d)
+        {
+            path = a;
+            title = b;
+            album = c;
+            artist = d;
+        }
+
+    }
+}
